beff94aeef94e835b209dafcea0395adcabc29e6
Seokjun7074 <sukjune96@gmail.com>
2023-10-23 16:49:55 +0900

    ✨ feat: 전역 스타일 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@/styles/theme';
+import GlobalStyle from '@/styles/globalStyle';
 
 const root = createRoot(document.querySelector('#root') as Element);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+const color = {
+  primary: '#F77137',
+  background: '#F8F8F8',
+  gray: '#D9D9D9',
+  lightgray: '#EBEBEB',
+  darkgray: '#454545',
+  red: '#F86375',
+  green: '#51CA87',
+  blue: '#6180D2',
+  yellow: '#FFD600',
+  black: '#000000',
+  white: '#ffffff',
+};
+
+const fontSize = {
+  s: '1.2rem',
+  m: '1.6rem',
+  l: '2.0rem',
+  xl: '3.5rem',
+};
+
+const borderRadius = {
+  s: '8px',
+  m: '16px',
+};
+
+export const theme = {
+  color,
+  fontSize,
+};
