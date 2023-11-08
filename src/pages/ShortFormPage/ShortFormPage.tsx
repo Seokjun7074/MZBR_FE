3db4ad66a5a2660ebf7a4d2134f0a62fd2905d04
Seokjun7074 <sukjune96@gmail.com>
2023-11-08 15:26:30 +0900

    ✨ feat: 영상 재생 페이지 라우터 연결

---
+++ src/pages/ShortFormPage/ShortFormPage.tsx
@@ -0,0 +1,7 @@
+import * as S from '@/pages/ShortFormPage/ShortFormPage.style';
+
+const ShortFormPage = () => {
+  return <S.ShortFormPageWrapper></S.ShortFormPageWrapper>;
+};
+
+export default ShortFormPage;
