4fbbbc19ef59c6fa7c2f0a685e2679d856fe18a2
이석준 <sukjune96@gmail.com>
2023-11-08 22:51:04 +0900

    ✨ feat: scroll snap 적용

--- src/pages/ShortFormPage/ShortFormPage.style.ts
+++ src/pages/ShortFormPage/ShortFormPage.style.ts
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
-import { Flex } from '@/components/common/Flex/Flex';
-
-export const ShortFormPageWrapper = styled(Flex)`
+export const ShortFormPageWrapper = styled.div`
   width: 100%;
   height: 100%;
-  background-color: black;
+  background-color: #0f0f0f;
+  overflow: auto;
+  scroll-snap-type: y mandatory;
 
   video {
     background-color: black;
   }
+`;
+
+export const ShotFormContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  scroll-snap-align: start;
+  padding: 5rem;
 `;
--- src/pages/ShortFormPage/ShortFormPage.tsx
+++ src/pages/ShortFormPage/ShortFormPage.tsx
@@ -13,24 +13,34 @@ const ShortFormPage = () => {
 
   return (
     <S.ShortFormPageWrapper>
-      <ReactPlayer
-        ref={videoRef}
-        onClick={handlePlay}
-        className="zzzzzzz"
-        url="https://joo-test-bucket-2023.s3.ap-northeast-2.amazonaws.com/encoded-video/c4730998-77c0-45d2-bb88-781e06d634fd/master.m3u8"
-        width="100%" // 플레이어 크기 (가로)
-        height="calc(100% - 2rem)" // 플레이어 크기 (세로)
-        playing={playing} // 자동 재생 on
-        muted={true} // 자동 재생 on
-        controls={false} // 플레이어 컨트롤 노출 여부
-        pip={true} // pip 모드 설정 여부
-        onEnded={() => {
-          alert('ZZ');
-        }}
-        onProgress={(e) => {
-          console.log(e);
-        }}
-      />
+      <S.ShotFormContainer>
+        <ReactPlayer
+          style={{ borderRadius: '8px', overflow: 'hidden' }}
+          ref={videoRef}
+          onClick={handlePlay}
+          className="zzzzzzz"
+          url="https://joo-test-bucket-2023.s3.ap-northeast-2.amazonaws.com/encoded-video/c4730998-77c0-45d2-bb88-781e06d634fd/master.m3u8"
+          width="100%"
+          height="100%"
+          playing={playing}
+          muted={true}
+          controls={true}
+        />
+      </S.ShotFormContainer>
+      <S.ShotFormContainer>
+        <ReactPlayer
+          ref={videoRef}
+          onClick={handlePlay}
+          className="zzzzzzz"
+          url="https://joo-test-bucket-2023.s3.ap-northeast-2.amazonaws.com/encoded-video/c4730998-77c0-45d2-bb88-781e06d634fd/master.m3u8"
+          width="100%" // 플레이어 크기 (가로)
+          height="100%" // 플레이어 크기 (세로)
+          playing={playing} // 자동 재생 on
+          muted={true} // 자동 재생 on
+          controls={true} // 플레이어 컨트롤 노출 여부
+          pip={true} // pip 모드 설정 여부
+        />
+      </S.ShotFormContainer>
     </S.ShortFormPageWrapper>
   );
 };
