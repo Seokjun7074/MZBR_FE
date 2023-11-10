ce7e9051e5fd6d9c2d1a63bb49cda847b68b811a
Seokjun7074 <sukjune96@gmail.com>
2023-11-10 17:48:24 +0900

    ✨ feat: 미리보기페이지 UI 개발중

--- src/pages/ReviewPage/VideoPreview/VideoPreview.style.ts
+++ src/pages/ReviewPage/VideoPreview/VideoPreview.style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { Button } from '@/components/common/Button/Button';
 import { Flex } from '@/components/common/Flex/Flex';
 
 export const VideoPreviewWrapper = styled(Flex)`
@@ -10,15 +11,40 @@ export const VideoPreviewWrapper = styled(Flex)`
   justify-content: space-between;
 `;
 
+export const PreviewSection = styled(Flex)`
+  width: 100%;
+  padding: 0 2rem;
+`;
+
+export const PreviewHeaderText = styled.h1`
+  color: ${({ theme }) => theme.color.primary};
+  font-size: 24px;
+  font-weight: bold;
+`;
+
 export const PreviewVideoContainer = styled(Flex)`
   max-height: 40rem;
   width: 100%;
-  background-color: black;
-  padding: 1rem 0;
 `;
 
 export const PreviewVideo = styled.video`
   width: auto;
   height: 100%;
   border-radius: 8px;
+`;
+
+export const ReviewTitleSubmitButton = styled(Button)`
+  background-color: ${({ theme }) => theme.color.primary};
+  border: none;
+  color: white;
+  padding: 8px 40px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 12px;
+  margin-top: 80px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #d65a28; // 색상을 조금 어둡게 변경
+  }
 `;
--- src/pages/ReviewPage/VideoPreview/VideoPreview.tsx
+++ src/pages/ReviewPage/VideoPreview/VideoPreview.tsx
@@ -6,17 +6,23 @@ import { preparedVideoAtom } from '@/store/video';
 
 const VideoPreview = () => {
   const preparedVideo = useRecoilValue(preparedVideoAtom);
-  console.log(preparedVideo);
 
   return (
     <S.VideoPreviewWrapper>
+      <S.PreviewHeaderText>이대로 업로드할까요?</S.PreviewHeaderText>
       <S.PreviewVideoContainer>
         <S.PreviewVideo
           crossOrigin="anonymous"
           autoPlay
+          controls
           src="https://mzbr-temp-video-bucket.s3.ap-northeast-2.amazonaws.com/crop/1c366c63-c45a-456c-9bd6-3249c6f44b0d.mp4"
-        ></S.PreviewVideo>
+        />
       </S.PreviewVideoContainer>
+      <S.PreviewSection>
+        <S.ReviewTitleSubmitButton>새 영상</S.ReviewTitleSubmitButton>
+        <S.ReviewTitleSubmitButton>음성/자막</S.ReviewTitleSubmitButton>
+        <S.ReviewTitleSubmitButton>제출</S.ReviewTitleSubmitButton>
+      </S.PreviewSection>
     </S.VideoPreviewWrapper>
   );
 };
