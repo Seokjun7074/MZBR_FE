c9d52a765bd4862e50891681c9575f15c99799d8
정선재 <amco1028@naver.com>
2023-11-11 10:36:03 +0900

    ✨ Feat: 프론트 axios와 페이지 연결
    내가 구독한 사용자 페이지와 axios 연결

--- src/apis/mypage/getMySubscribedUsers.ts
+++ src/apis/mypage/getMySubscribedUsers.ts
@@ -3,84 +3,47 @@ import { useNavigate } from 'react-router-dom';
 
 import axios from 'axios';
 
+import { ApiResponse, SubscribedUser, User } from '../../pages/MyPage/Social/SubscribedUsers';
 import { getNewAccessToken } from '../getNewAccessToken';
 
 const BASE_URL = 'http://localhost:3000';
 const navigate = useNavigate();
-const defaultSubscribes = [
-  {
-    userId: '1',
-    profileImage: 'sad',
-    nickname: 'sada',
-  },
-  {
-    userId: '2',
-    profileImage: 'ssaad',
-    nickname: 'saasdda',
-  },
-];
 
-type User = {
-  userId: number;
-  accessToken: string;
-  refreshToken: string;
-};
+export const useSubscribedUsers = (
+  user: User,
+  setUser: React.Dispatch<React.SetStateAction<User>>,
+) => {
+  const [subscribes, setSubscribes] = useState<SubscribedUser[]>([]);
+  const navigate = useNavigate();
 
-const [subscribes, setSubscribes] = useState(defaultSubscribes);
-const [user, setUser] = useState<User>({
-  userId: 1,
-  accessToken: 'some_initial_access_token',
-  refreshToken: 'some_initial_refresh_token',
-});
+  useEffect(() => {
+    const fetchSubscribe = async () => {
+      try {
+        const response = await axios.get<ApiResponse<SubscribedUser[]>>(
+          `${BASE_URL}/users/subscriber`,
+          {
+            headers: { 'access-token': user.accessToken },
+          },
+        );
 
-useEffect(() => {
-  const fetchSubscribe = async () => {
-    type subscribes = {
-      userId: string;
-      profileImage: string;
-      nickname: string;
-    };
-
-    type ApiResponse<T> = {
-      success: boolean;
-      data: T;
-      error?: string;
-    };
-
-    try {
-      const response = await axios.get<ApiResponse<subscribes[]>>(BASE_URL + '/users/subscriber', {
-        headers: { 'access-token': user.accessToken },
-      });
-
-      if (response.data.success) {
-        setSubscribes(response.data.data);
-      } else if (response.data.error && response.data.error === 'JWT_TOKEN_EXPIRED_EXCEPTION') {
-        const newAccessToken = await getNewAccessToken(user.refreshToken);
-        if (newAccessToken) {
-          setUser({ ...user, accessToken: newAccessToken });
-
-          const newResponse = await axios.get<ApiResponse<subscribes[]>>(
-            BASE_URL + '/mypage/myvideo',
-            {
-              headers: { 'access-token': newAccessToken },
-            },
-          );
-
-          if (newResponse.data.success) {
-            setSubscribes(newResponse.data.data);
+        if (response.data.success) {
+          setSubscribes(response.data.data);
+        } else if (response.data.error === 'JWT_TOKEN_EXPIRED_EXCEPTION') {
+          const newAccessToken = await getNewAccessToken(user.refreshToken);
+          if (newAccessToken) {
+            setUser((prev) => ({ ...prev, accessToken: newAccessToken }));
+            // Retry the request with the new access token
           } else {
             navigate('/error');
           }
-        } else {
-          navigate('/error');
         }
+      } catch (error) {
+        navigate('/error');
       }
-    } catch (error) {
-      navigate('/error');
-    }
-  };
+    };
 
-  if (user) {
     fetchSubscribe();
-  }
-}, [user, setUser, navigate]);
+  }, [user, setUser, navigate]);
+
+  return subscribes;
+};
--- src/pages/MyPage/Social/SubscribedUsers.tsx
+++ src/pages/MyPage/Social/SubscribedUsers.tsx
@@ -1,34 +1,44 @@
+import React, { useState } from 'react';
+
+import { useSubscribedUsers } from '@/apis/mypage/getMySubscribedUsers';
+
 import Profile from '../../../assets/Profile.png';
 import { Button, Container, Header, UserItem } from './SubscribedUsers.style';
 
-interface User {
-  id: number;
-  image: string;
+export interface User {
+  userId: number;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface SubscribedUser {
+  userId: string;
+  profileImage: string;
   nickname: string;
 }
 
-const DUMMY_USERS: User[] = [
-  { id: 1, image: Profile, nickname: 'User1' }, // image 경로를 Profile로 변경
-  { id: 2, image: Profile, nickname: 'User2' },
-  { id: 3, image: Profile, nickname: 'User3' },
-  { id: 4, image: Profile, nickname: 'User4' },
-  { id: 5, image: Profile, nickname: 'User5' },
-  { id: 6, image: Profile, nickname: 'User6' },
-  { id: 7, image: Profile, nickname: 'User7' },
-  { id: 8, image: Profile, nickname: 'User8' },
-];
+export interface ApiResponse<T> {
+  success: boolean;
+  data: T;
+  error?: string;
+}
 
-// React 컴포넌트 부분
-// 나중에 구독한 사용자 정보 가져와서 만들기
-// 나중에 구독취소 버튼 누르면 작동하도록 구현하기
-const SubcribedUsers = () => {
+const SubscribedUsers = () => {
+  const [user, setUser] = useState<User>({
+    userId: 1,
+    accessToken: 'some_initial_access_token',
+    refreshToken: 'some_initial_refresh_token',
+  });
+
+  const subscribes = useSubscribedUsers(user, setUser);
+
   return (
     <Container>
       <Header>구독중인 사용자 목록</Header>
-      {DUMMY_USERS.map((user: User) => (
-        <UserItem key={user.id}>
-          <img src={user.image} alt={user.nickname} />
-          <span>{user.nickname}</span>
+      {subscribes.map((subscribe: SubscribedUser) => (
+        <UserItem key={subscribe.userId}>
+          <img src={subscribe.profileImage} alt={subscribe.nickname} />
+          <span>{subscribe.nickname}</span>
           <Button>구독취소</Button>
         </UserItem>
       ))}
@@ -36,4 +46,4 @@ const SubcribedUsers = () => {
   );
 };
 
-export default SubcribedUsers;
+export default SubscribedUsers;
