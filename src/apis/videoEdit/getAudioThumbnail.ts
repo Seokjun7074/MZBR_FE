5aca4ef8f5c776f1031477d6f4376a8f8567dc8d
Seokjun7074 <sukjune96@gmail.com>
2023-11-13 13:41:33 +0900

    ✏️ fix: PresignURL 요청 API 수정

--- src/apis/videoEdit/getAudioThumbnail.ts
+++ src/apis/videoEdit/getAudioThumbnail.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from '@/apis';
 
 export const getAudioThumbnail = async (thumbnailFileName: string, audioFileName?: string) => {
-  const { data } = await axiosInstance.post('/videos/upload/complete', {
+  const { data } = await axiosInstance.post('/api/v/video/thumbnail-and-audio-upload-url', {
     thumbnailFileName,
     audioFileName,
   });
--- src/pages/ReviewPage/VideoPreview/VideoPreview.tsx
+++ src/pages/ReviewPage/VideoPreview/VideoPreview.tsx
@@ -1,3 +1,4 @@
+import { fetchFile } from '@ffmpeg/ffmpeg';
 import { v4 } from 'uuid';
 
 import { useEffect, useState } from 'react';
@@ -6,11 +7,15 @@ import { useRecoilValue } from 'recoil';
 
 import * as S from '@/pages/ReviewPage/VideoPreview/VideoPreview.style';
 
+import { useFFmpeg } from '@/hooks/useFFmpeg';
+
+import { getAudioThumbnail } from '@/apis/videoEdit/getAudioThumbnail';
 import { getPreviewVideo } from '@/apis/videoEdit/getPreviewViewo';
 
 import { preparedVideoAtom } from '@/store/video';
 
 const VideoPreview = () => {
+  const { ffmpegRef } = useFFmpeg();
   const preparedVideoState = useRecoilValue(preparedVideoAtom);
   const [videoPreview, setVideoPreview] = useState<string>('');
 
@@ -24,6 +29,35 @@ const VideoPreview = () => {
     };
     // fetchPreviewUrl();
   }, []);
+
+  const makeThumbnail = async () => {
+    const ffmpeg = ffmpegRef.current;
+    if (!ffmpeg) return;
+    const url =
+      'https://mzbr-temp-video-bucket.s3.ap-northeast-2.amazonaws.com/crop/2ac6fe92-cb3c-4de7-b6bb-1d77ed25e524.mp4';
+    // const url = preparedVideoState[0].videoUrl;
+    const thumbnailName = `${v4()}.jpeg`;
+
+    ffmpeg.FS('writeFile', `inputVideo.mp4`, await fetchFile(url));
+    try {
+      await ffmpeg.run('-i', 'inputVideo.mp4', '-ss', '00:00:00', '-vframes', '1', thumbnailName);
+    } catch (e) {
+      console.error('[썸네일 생성 오류]', e);
+    }
+    const result = ffmpeg.FS('readFile', thumbnailName);
+    const blob = new Blob([result.buffer], { type: 'image/jpeg' });
+    // const dataUrl = URL.createObjectURL(blob);
+    return { blob, thumbnailName };
+  };
+
+  const handleSubmit = async () => {
+    const result = await makeThumbnail();
+    console.log(result);
+    if (result?.thumbnailName) {
+      const presignUrl = await getAudioThumbnail(result.thumbnailName, '');
+      console.log('presignUrl', presignUrl);
+    }
+  };
 
   return (
     <S.VideoPreviewWrapper>
@@ -41,7 +75,9 @@ const VideoPreview = () => {
         <S.ReviewTitleSubmitButton>음성 / 자막 추가</S.ReviewTitleSubmitButton>
       </S.PreviewSection>
       <S.PreviewSection>
-        <S.ReviewTitleSubmitButton>이대로 업로드 할래요</S.ReviewTitleSubmitButton>
+        <S.ReviewTitleSubmitButton onClick={handleSubmit}>
+          이대로 업로드 할래요
+        </S.ReviewTitleSubmitButton>
       </S.PreviewSection>
     </S.VideoPreviewWrapper>
   );
