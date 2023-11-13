b7b7c701df74946b1ea44f1e33ec4081cf0e141a
정선재 <amco1028@naver.com>
2023-11-13 12:25:54 +0900

    ✨ Feat: 구독버튼 추가
    다른 유저 페이지 들어가서 구독할 수 있는 구독 하기 버튼 추가

--- src/pages/MyPage/User.tsx
+++ src/pages/MyPage/User.tsx
@@ -1,6 +1,7 @@
-// MyUserPage.tsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import axios from 'axios';
 
 import { useMyVideos } from '../../apis/Video/getUserVideo';
 import { useUserProfile } from '../../apis/mypage/getUserInfo';
@@ -38,6 +39,8 @@ export interface ApiResponse<T> {
   error?: string;
 }
 
+const BASE_URL = 'http://localhost:3000';
+
 const MyUserPage = () => {
   const { userId } = useParams<{ userId?: string }>();
   const parsedUserId = userId ? parseInt(userId, 10) : null;
@@ -55,15 +58,33 @@ const MyUserPage = () => {
     postNum: 0,
   });
 
-  const [videos, setVideos] = useState<Video[]>([]); // 동영상 목록 상태 추가
+  const [videos, setVideos] = useState<Video[]>([]);
 
   useEffect(() => {
     if (parsedUserId !== null && !isNaN(parsedUserId)) {
       useUserProfile(parsedUserId, user, setUser, setUserProfile);
-      const fetchedVideos = useMyVideos(parsedUserId, user, setUser);
-      setVideos(fetchedVideos); // 동영상 목록 상태 업데이트
+      useMyVideos(parsedUserId, user, setUser);
     }
   }, [parsedUserId, user.accessToken]);
+
+  const handleSubscribe = async () => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/users/subscribe/${parsedUserId}`,
+        {},
+        { headers: { 'access-token': user.accessToken } },
+      );
+
+      if (response.data.success) {
+        console.log('구독 성공');
+      } else {
+        console.log('구독 실패:', response.data.error);
+      }
+    } catch (error) {
+      console.error('구독 중 오류 발생:', error);
+    }
+  };
+
   return (
     <PageWrapper>
       <ProfileSection>
@@ -75,14 +96,15 @@ const MyUserPage = () => {
           <h3>{userProfile.nickname}</h3>
           <p>구독자: {userProfile.subscribeNum}</p>
           <p>게시물: {userProfile.postNum}</p>
+          <button onClick={handleSubscribe}>구독하기</button>
         </UserInfo>
       </ProfileSection>
+
       <VideosGrid>
         {videos.map((video) => (
           <VideoThumbnail key={video.id} src={video.thumbnail_url} alt="Video Thumbnail" />
         ))}
       </VideosGrid>
-      s
     </PageWrapper>
   );
 };
