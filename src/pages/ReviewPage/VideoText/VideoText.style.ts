30e2b4767f291ffb02395257899121b6dced7c22
Seokjun7074 <sukjune96@gmail.com>
2023-11-14 11:09:19 +0900

    ✨ feat: 자막 react-Rnd 추가

--- src/pages/ReviewPage/VideoText/VideoText.style.ts
+++ src/pages/ReviewPage/VideoText/VideoText.style.ts
@@ -1,9 +1,53 @@
 import styled from 'styled-components';
 
+import { Button } from '@/components/common/Button/Button';
 import { Flex } from '@/components/common/Flex/Flex';
 
 export const VideoTextWrapper = styled(Flex)`
   width: 100%;
   height: 100%;
   flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+export const VideoContainer = styled(Flex)`
+  width: 100%;
+  height: 40rem;
+  flex-direction: column;
+  background-color: black;
+  border-radius: 8px;
+`;
+
+export const VideoTextOverlay = styled(Flex)`
+  position: relative;
+`;
+
+export const VideoTag = styled.video`
+  width: auto;
+  height: 40rem;
+  background-color: black;
+`;
+
+export const TextInput = styled.input`
+  width: 20rem;
+  padding: 1rem;
+  background-color: ${({ theme }) => theme.color.lightgray};
+  color: ${({ theme }) => theme.color.darkgray};
+  font-weight: bold;
+  border-radius: 8px;
+`;
+
+export const VideoTextButton = styled(Button)`
+  background-color: ${({ theme }) => theme.color.primary};
+  border: none;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: bold;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #d65a28; // 색상을 조금 어둡게 변경
+  }
 `;
--- src/pages/ReviewPage/VideoText/VideoText.tsx
+++ src/pages/ReviewPage/VideoText/VideoText.tsx
@@ -1,13 +1,49 @@
+import { useRef } from 'react';
+import { Rnd } from 'react-rnd';
+
 import { useRecoilState } from 'recoil';
 
 import * as S from '@/pages/ReviewPage/VideoText/VideoText.style';
 
+import { useInput } from '@/hooks/useInput';
+
 import { preparedVideoAtom } from '@/store/video';
 
 const VideoText = () => {
-  const [preparedVideo, setPreparedVideo] = useRecoilState(preparedVideoAtom);
+  const DUMMY_VIDEO =
+    'https://mzbr-temp-video-bucket.s3.ap-northeast-2.amazonaws.com/crop/2ac6fe92-cb3c-4de7-b6bb-1d77ed25e524.mp4';
 
-  return <S.VideoTextWrapper></S.VideoTextWrapper>;
+  const [preparedVideo, setPreparedVideo] = useRecoilState(preparedVideoAtom);
+  const rndRef = useRef<Rnd | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const { value, handleInput } = useInput('');
+
+  return (
+    <S.VideoTextWrapper>
+      <S.VideoContainer>
+        <S.VideoTextOverlay>
+          <S.VideoTag ref={videoRef} src={DUMMY_VIDEO} crossOrigin="anonymous" autoPlay controls />
+          {/* <S.VideoTag ref={videoRef} src={preparedVideo[0].videoUrl} /> */}
+          <Rnd
+            ref={rndRef}
+            style={{ border: '1px solid red' }}
+            bounds={'parent'}
+            default={{
+              x: 0,
+              y: 0,
+              width: 'auto',
+              height: 'auto',
+            }}
+            enableResizing={false}
+          >
+            {value}
+          </Rnd>
+        </S.VideoTextOverlay>
+      </S.VideoContainer>
+      <S.TextInput type="text" placeholder="자막을 추가해주세요" onChange={handleInput} />
+      <S.VideoTextButton>자막 추가하기</S.VideoTextButton>
+    </S.VideoTextWrapper>
+  );
 };
 
 export default VideoText;
