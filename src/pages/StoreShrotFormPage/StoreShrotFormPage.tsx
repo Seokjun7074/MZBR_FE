ce4497d8264a14634e49070a0b95111cc61532a2
이석준 <sukjune96@gmail.com>
2023-11-15 21:49:14 +0900

    ✨ feat: 데이터 없는 경우 처리

--- src/pages/StoreShrotFormPage/StoreShrotFormPage.tsx
+++ src/pages/StoreShrotFormPage/StoreShrotFormPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import * as S from '@/pages/StoreShrotFormPage/StoreShrotFormPage.style';
 
@@ -6,15 +6,30 @@ import ShotFormContainer from '@/components/ShortForm/ShotFormContainer/ShotForm
 
 import { useRestaurantVideoListQuery } from '@/hooks/queries/useRestaurantVideoListQuery';
 
+import { PATH } from '@/constants/path';
+
 import { VideoInfo } from '@/types/shortForm';
 
 const StoreShrotFormPage = () => {
   const { storeId } = useParams();
+  const navigate = useNavigate();
   const { restaurantVideoListData } = useRestaurantVideoListQuery(storeId!);
+
+  if (restaurantVideoListData?.videos.length < 1) {
+    return (
+      <S.EmptyReviewContainer>
+        <h1>등록된 리뷰가 없네요 😅</h1>
+        <span>첫 리뷰를 등록해주세요!</span>
+        <S.ReviewButton onClick={() => navigate(PATH.REVIEW(storeId!))}>
+          ✏️ 첫 리뷰 등록하기
+        </S.ReviewButton>
+      </S.EmptyReviewContainer>
+    );
+  }
 
   return (
     <S.StoreShrotFormPageWrapper>
-      {restaurantVideoListData?.videoDtos.map((item: VideoInfo) => (
+      {restaurantVideoListData?.videos.map((item: VideoInfo) => (
         <ShotFormContainer key={item.id} videoInfo={item} />
       ))}
     </S.StoreShrotFormPageWrapper>
