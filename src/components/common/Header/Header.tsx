cdea1887e3de0fd54d25dc501c60a368685ace81
Seokjun7074 <sukjune96@gmail.com>
2023-11-16 09:34:57 +0900

    ✨ feat: 헤더 버튼 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,7 +1,28 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import * as S from '@/components/common/Header/Header.style';
 
+import { PATH } from '@/constants/path';
+
+import BackArrow from '@/assets/navigationBar/backArrow.svg';
+import MypageButton from '@/assets/navigationBar/mypage_button.svg';
+
 const Header = () => {
-  return <S.HeaderWrapper></S.HeaderWrapper>;
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  if (pathname === PATH.MAP) {
+    return (
+      <S.HeaderWrapper $isMap={true}>
+        <MypageButton />
+      </S.HeaderWrapper>
+    );
+  }
+  return (
+    <S.HeaderWrapper $isMap={false}>
+      <BackArrow onClick={() => navigate(-1)} />
+    </S.HeaderWrapper>
+  );
 };
 
 export default Header;
