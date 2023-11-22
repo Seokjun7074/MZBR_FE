4bd639a96f4ef0c94eaa2bf92ee6e594bcadb09a
Seokjun7074 <sukjune96@gmail.com>
2023-11-22 15:42:32 +0900

    🎨 refactor: interceptor 분리

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,6 @@
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
-import { PATH } from '@/constants/path';
+import { addAccessToken, handelRefreshToken } from '@/apis/interceptor';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.SERVER_URL,
@@ -10,41 +10,8 @@ export const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('Authorization')}`;
-    config.headers['member-id'] = `${localStorage.getItem('userId')}`;
-    return config;
-  },
-  (err) => {
-    return Promise.reject(err);
-  },
-);
+axiosInstance.interceptors.request.use(addAccessToken, (err) => {
+  return Promise.reject(err);
+});
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    const { data } = error.response;
-
-    if (data.status === 401 && data.errorCode === 'Unauthorized') {
-      const refreshToken = localStorage.getItem('Authorization-refresh');
-      if (refreshToken) {
-        originalRequest.headers['Authorization-refresh'] = `Bearer ${refreshToken}`;
-        const newResponse = await axiosInstance(originalRequest);
-        delete originalRequest.headers['Authorization-refresh'];
-        localStorage.setItem('Authorization', newResponse.data.Authorization);
-        localStorage.setItem('Authorization-refresh', newResponse.data['Authorization-refresh']);
-        originalRequest.headers.Authorization = `Bearer ${newResponse.data.Authorization}`;
-        return axiosInstance(originalRequest);
-      }
-      alert('인증이 만료되었습니다. 다시 로그인해주세요');
-      window.location.replace(PATH.ROOT);
-      return Promise.reject(error);
-    }
-
-    return Promise.reject(error);
-  },
-);
+axiosInstance.interceptors.response.use((response) => response, handelRefreshToken);
